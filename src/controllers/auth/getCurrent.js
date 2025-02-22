7dbf6cca1502e24f3b006495635e0d7cd51458eb
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-22 14:58:31 +0200

    getCurrent

---
+++ src/controllers/auth/getCurrent.js
@@ -0,0 +1,16 @@
+
+export const getCurrent = async (req, res) => {
+  try {
+    const user = req.user
+
+    const { password: _, ...plainUser } = user.toObject()
+
+    res.status(200).json({
+      message: "Splendid",
+      user : plainUser
+    });
+  } catch (error) {
+    console.log('Error in user getCurrent', error);
+    res.status(500).json({ message: 'Server error' })
+  }
+}
