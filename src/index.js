4fb28c7d74044d20091272a9b8a335d4f8e0c462
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-22 13:38:11 +0200

    removed all ts Dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from './routes/auth.route.js'
 
 // Load environment variables
 dotenv.config();
@@ -11,11 +12,14 @@ const port = process.env.PORT || 5500;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Sample route
 app.get("/", (req, res) => {
   res.send("Hello World from Express API with TypeScript!");
 });
+
+app.use('/api/auth', authRoutes);
 
 // Start the server
 app.listen(port, () => {
