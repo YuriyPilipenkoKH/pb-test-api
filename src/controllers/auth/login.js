a90c8af507e8a144ccc462d9eadb96b1bc573a81
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-22 15:13:40 +0200

    login

---
+++ src/controllers/auth/login.js
@@ -0,0 +1,50 @@
+import { compareSync } from "bcryptjs";
+import { User } from "../../models/user.model.js";
+import capitalize from "../../lib/capitalize.js";
+
+
+
+export const login = async (req, res) => {
+
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+    if (!user) {
+      res.status(401).json({
+        message: "Invalid credentials",
+        credentialsError: true,
+        errorCode: 'credentialsError',
+      });
+      return;
+    }
+
+    const passwordCompare = compareSync(password, user.password)
+    if (!passwordCompare) {
+      res.status(401).json({
+        message: "Invalid credentials",
+        credentialsError: true,
+        errorCode: 'credentialsError',
+      });
+      return;
+    }
+
+    const payload = {
+      id: user._id,
+    }
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: "1y" })
+
+      const { password: _, ...plainUser } = user.toObject()
+
+      res.status(200).json({
+        message: `Welcome back, ${capitalize(plainUser.name)}`,
+        success: true,
+        user: plainUser,
+        token,
+      });
+    
+  } catch (error) {
+    console.log('Error in user login', error);
+    res.status(500).json({ message: 'Server error' })
+   }
+}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { login } from '../controllers/auth/login.js';
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json({ message: "Auth route is working!" });
 });
+
+router.post('/login', login );
 
 export default router;
